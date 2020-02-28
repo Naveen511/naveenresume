cd5ae4c79c4f9d42b440990fd8e37b9a99af86bf
Naveen <ptnaveenraj@outlook.com>
2020-02-28 09:30:59 +0530

    InitialCommit

---
+++ script.js
@@ -0,0 +1,169 @@
+var objective = ["Energetic software engineer with 3 years of experiences"
+    + "developing robust code for high-volume businesses.",
+    "Participated in user requirements gathering, project planning, design, implementation and testing.",
+    "An excellent problem-solver, able to quickly grasp complex systems"
+    + " identify opportunities for improvements and resolution of critical issues.",
+    "I am goal-oriented, self-motivated and committed to the successful outcome of the project."
+]
+// Generate the li tag and set in html page
+objective.forEach(function(item) {
+    var li = document.createElement("li");
+    var text = document.createTextNode(item);
+    li.appendChild(text);
+    document.getElementById("objective").appendChild(li);
+});
+// Set the wipadika company exp in array
+var wipadikaExp = [
+    "Developed an User Interactive web design on multiple projects.",
+    "Standardized all output with a new, responsive, mobile-first approach and strategy.",
+    "Collaborated with product team members to implement new feature developments.",
+    "Explored various emerging JavaScript technologies for future product and feature development.",
+    "Made heavy use of Chrome Developer Tools + Firebug to ensure front-end"
+    +" code and assets were optimized and high-quality.",
+    "Developed wireframes and mockups using for application storyboards and development.",
+    "Integration of mockups and as well as with the live apis to make thed esign as an optimized one.",
+    "Worked on Reactjs frameworks with some styling	boosters such as material UI, React rainbow	components.",
+    "Works on AWS featues such as aws-amplify, aws-stepfunction."
+]
+// Generate the li tag and set in html page
+wipadikaExp.forEach(function(item) {
+    var li = document.createElement("li");
+    var text = document.createTextNode(item);
+    li.appendChild(text);
+    document.getElementById("wipadika_exp").appendChild(li);
+});
+
+// Set the nichehands company exp in array
+var nicheExp = [
+    "Developed a complete Jhipster web application to make the user"+
+        " to reduce the expenditure 3/4th of amount spend in Nursery.",
+    "Developed Interactive e-learning Platform for Students and "+
+        "Teacher Training Program and make them to Mapping Predictive Education.",
+    "Worked on 360* Collaborative Learning Staffs,"+
+        " Parent predictive collaborative coaching and guidance system.",
+    "Built a real time mobile application for tracking the user exact location"+
+        " which includes all the features like traveled route, "+
+        "kms traveled Attendance status on reaching	location, trip status etc.",
+    "Built a real time mobile application for the video learning which includes "+
+        "the writing exam at each interval based on	the	waiting	time and the exam English result.",
+    "Work on small to complex development projects, including analysis,"+
+    "Tamil coding, testing and implementation providing automated solutions to"+
+        "business accounting processes.",
+    "Creative in Designing the UI that is appealing and also gives user friendly experience.",
+    "Experience with integration services Keycloak, Docker.",
+    "Worked on client-side options such as HTML, Bootstrap CSS, SASS,"+
+        "Yarn, Angular 6, TypeScript.",
+    "Worked on server-side options such as SpringBoot, Gradle, Maven,"+
+    "Hibernate, Liquibase, Mysql, Postgresql, Thymeleaf, Elastic search."
+]
+// Generate the li tag and set in html page
+nicheExp.forEach(function(item) {
+    var li = document.createElement("li");
+    var text = document.createTextNode(item);
+    li.appendChild(text);
+    document.getElementById("niche_exp").appendChild(li);
+});
+
+// lobb experience
+var lobbExp = [
+    "Creative in Designing the UI that is appealing and also gives"+
+    " user friendly experience.",
+    "Participated in user requirements gathering, project planning, design,"+
+    " implementation and testing.",
+    "Execute various visual design stages from concept to final hand-off to"+
+    " development.",
+    "Troubleshooting and defect fixing.",
+]
+// Generate the li tag and set in html page
+lobbExp.forEach(function(item) {
+    var li = document.createElement("li");
+    var text = document.createTextNode(item);
+    li.appendChild(text);
+    document.getElementById("lobb_exp").appendChild(li);
+});
+
+// molar byte experience
+var molarByteExp = [
+    "Develop responsive front end pages, participating in the design of the "+
+    "product and understanding the business requirements.",
+    "Designing, documenting and implementing technical requirements for "+
+    "business applications.",
+    "Developing and maintaining web and front ends for various browsers and "+
+    "devices.",
+    "Using of design tool to create prototype and then implement it into the "+
+    "application.",
+    "Integrated some of the npm package components to make the application "+
+    "looks pretty good.",
+    "Troubleshooting and defect fixing."
+]
+// Generate the li tag and set in html page
+molarByteExp.forEach(function(item) {
+    var li = document.createElement("li");
+    var text = document.createTextNode(item);
+    li.appendChild(text);
+    document.getElementById("molar_byte_exp").appendChild(li);
+});
+
+// ilearn experience
+var greenHandsExp = [
+    "Initiate and manage projects that provide new solutions and "+
+    "improvements.",
+    "Participated in full software development life-cycle like user requirements "+
+    "gathering, project planning, design, implementation and testing,",
+    "Participated in various client meetings with the team and the project "+
+    "manager to discuss improvements in the development of the application.",
+    "Test planning, unit testing, troubleshooting and defect fixing.",
+    "Work on the integration of Keycloak, Docker."
+]
+// Generate the li tag and set in html page
+greenHandsExp.forEach(function(item) {
+    var li = document.createElement("li");
+    var text = document.createTextNode(item);
+    li.appendChild(text);
+    document.getElementById("greenhands_exp").appendChild(li);
+});
+
+// ilearn experience
+var ilearnExp = [
+    "Initiate and manage projects that provide new solutions and "+
+    "improvements.",
+    "Test planning, unit testing,troubleshooting and defect fixing.",
+    "Perform initial debugging procedures by reviewing configuration files, logs "+
+    "and code pieces to determine breakdown source.",
+    "Correct errors by making appropriate changes and rechecking the "+
+    "program to ensure that the desired results are produced."
+]
+// Generate the li tag and set in html page
+ilearnExp.forEach(function(item) {
+    var li = document.createElement("li");
+    var text = document.createTextNode(item);
+    li.appendChild(text);
+    document.getElementById("ilearn_exp").appendChild(li);
+});
+
+// 3t-tracking experience
+var trackingExp = [
+    "Participated in user requirements gathering, project planning, design, "+
+    "implementation and testing.",
+    "Creative in Designing the UI that is appealing and also gives user friendly "+
+    "experience.",
+    "Conduct trial run of programs, software applications to be sure they will "+
+    "produce the desired information and that the instructions are correct.",
+    "Live tracking by making appropriate changes and rechecking the program "+
+    "to ensure that the desired results are produced."
+]
+// Generate the li tag and set in html page
+trackingExp.forEach(function(item) {
+    var li = document.createElement("li");
+    var text = document.createTextNode(item);
+    li.appendChild(text);
+    document.getElementById("3ttrack_exp").appendChild(li);
+});
+
+// Generate the li tag and set in html page
+rewardAchieve.forEach(function(item) {
+    var li = document.createElement("li");
+    var text = document.createTextNode(item);
+    li.appendChild(text);
+    document.getElementById("reward_achieve").appendChild(li);
+});
